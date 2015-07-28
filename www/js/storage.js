e98d4f11b1b46601f8cea837d416d0fc3e768a51
Kevin Howell <kevin@kahowell.net>
2015-07-27 22:11:25 -0400

    Rewrite storage impl in PouchDB

--- www/js/storage.js
+++ www/js/storage.js
@@ -1,87 +1,61 @@
 angular.module('ryouri_book')
     .factory('recipe_service', ['$q', function($q) {
-        var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB;
-        var request = indexedDB.open('recipes', 1);
-        var recipe_store_deferred = $q.defer();
-        var recipe_store_promise = recipe_store_deferred.promise;
-        request.onupgradeneeded = function(event) {
-            // initialize indexedDB
-            var db = event.target.result;
-            var recipe_store = db.createObjectStore('recipes', { keyPath: 'name' });
-            recipe_store.createIndex('keywords', 'keywords', { multiEntry: true });
-        }
-        request.onsuccess = function(event) {
-            var db = event.target.result;
-            recipe_store_deferred.resolve(db);
+        var initialized_db_deferred = $q.defer();
+        var recipe_store_promise = initialized_db_deferred.promise;
+        var db = new PouchDB('recipes');
+        /*
+        var keyword_index = {
+            _id: '_design/keyword_idx',
+            views: {
+                'keyword_idx': {
+                    map: function (recipe) {
+                        recipe.keywords.each(function(keyword)) {
+                            emit(keyword);
+                        }.toString();
+                    }
+                }
+            }
         };
+        */
+        //pouch.put(keyword_index).then(function() {
+            initialized_db_deferred.resolve(db);
+        //});
         this.add_recipe = function(recipe) {
-            // gather keywords
-            recipe.keywords = [];
+            recipe._id = recipe.name;
+            recipe.keywords = [recipe.name];
             recipe.ingredients.map(function(ingredient) {
                 recipe.keywords.push(ingredient.name);
             });
-            recipe.keywords.push(recipe.name);
-            var add_recipe_deferred = $q.defer();
-            recipe_store_promise.then(function(db) {
-                var recipe_store = db.transaction(['recipes'], 'readwrite').objectStore('recipes');
-                recipe_store.add(recipe).onsucess = function() {
-                    add_recipe_deferred.resolve();
-                };
-            });
-            return add_recipe_deferred.promise;
+            return $q.when(db.put(recipe));
         }
         this.load_recipes = function() {
-            var load_recipes_deferred = $q.defer();
-            var recipes = [];
-            recipe_store_promise.then(function(db) {
-                var recipe_store = db.transaction(['recipes'], 'readonly').objectStore('recipes');
-                var req = recipe_store.openCursor();
-                req.onsuccess = function(event) {
-                    var cursor = event.target.result;
-                    if (cursor) {
-                        recipes.push(cursor.value);
-                        cursor.continue();
-                        //load_recipes_deferred.notify(recipes);
-                    }
-                    else {
-                        load_recipes_deferred.resolve(recipes);
-                    }
-                }
-                req.onerror = function(event) {
-                    // TODO handle
-                }
+            var recipes_deferred = $q.defer();
+            db.allDocs({include_docs: true}).then(function(results) {
+                var recipes = [];
+                results.rows.map(function(row) {
+                   recipes.push(row.doc);
+                });
+                recipes_deferred.resolve(recipes);
             });
-            return load_recipes_deferred.promise;
+            return recipes_deferred.promise;
         };
         this.load_recipe = function(recipe_name) {
-            var load_recipe_deferred = $q.defer();
-            recipe_store_promise.then(function(db) {
-                var recipe_store = db.transaction(['recipes'], 'readonly').objectStore('recipes');
-                recipe_store.get(recipe_name).onsuccess = function(event) {
-                    load_recipe_deferred.resolve(event.target.result);
-                };
-            });
-            return load_recipe_deferred.promise;
+            return $q.when(db.get(recipe_name));
         };
-        this.search = function(keyword) {
-            var search_deferred = $q.defer();
-            var recipes = [];
-            recipe_store_promise.then(function(db) {
-                var recipe_store = db.transaction(['recipes'], 'readonly').objectStore('recipes');
-                var index = recipe_store.index('keywords');
-                index.openCursor(IDBKeyRange.only(keyword)).onsuccess = function(event) {
-                    var cursor = event.target.result;
-                    if (cursor) {
-                        recipes.push(cursor.value);
-                        cursor.continue();
-                        search_deferred.notify(recipes);
-                    }
-                    else {
-                        search_deferred.resolve(recipes);
-                    }
-                };
+        this.search = function(query) {
+            var recipes_deferred = $q.defer();
+            db.search({
+                query: query,
+                fields: ['name', 'keywords'],
+                include_docs: true
+            }, function(err, results) {
+                var recipes = [];
+                results.rows.map(function(row) {
+                    recipes.push(row.doc);
+                });
+                recipes_deferred.resolve(recipes);
             });
-            return search_deferred.promise;
+            return recipes_deferred.promise;
         }
         return this;
     }]);
